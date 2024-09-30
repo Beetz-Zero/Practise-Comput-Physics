a0603436b51bbc0a00494e2804f32ff2ceab9d71
Beetz <olegson188@gmail.com>
2024-09-30 20:58:03 +1000

    Первый коммит

--- Program1/main.cpp
+++ Program1/main.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
+int main(){
+    std::ifstream file("D:/Programmin/C++/Practise-Comput-Physics/XMCD#0000..txt");
+    if (!file.is_open()){
+        cout<<"File isn't initialised"<<endl;
+    }
+    
+}
