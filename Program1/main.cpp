294664bf38a2d02ba42f12beffe5a90be1a425a7
Beetz <olegson188@gmail.com>
2024-10-02 20:06:48 +1000

    Добавил счёт строк и немного насрал

--- Program1/main.cpp
+++ Program1/main.cpp
@@ -4,17 +4,45 @@
 #include <math.h>
 using namespace std;
 
+int countString(string Path);
+struct spin
+{
+    float x;
+    float y;
+    float spin;
+};
+
+
 int main(){
-    std::ifstream file("D:/Programmin/C++/Practise-Comput-Physics/XMCD#0000..txt");
-    if (!file.is_open()){
-        cout<<"File isn't initialised"<<endl;
-    }
-    int N;
-    cout<<"Enter Number of spins"<<endl;
-    cin>>N;
-    for (int i = 0; i < N; i++){
+    string Path;
+    cout<<"Enter path to source data: ";
+    cin >> Path;
+    int N = countString(Path);
+    ifstream file(Path);
+    string line;
+    spin spin1;
+    spin spin2;
+    
+    cout << line;
+    /*for (int i = 0; i < N; i++){
         for (int i = 0; i < N; i++){
             cout<<"bruh"<<endl;
         }
     }
+    */
+}
+
+int countString(string Path){
+    ifstream file(Path);
+    int N = 0;
+    if (file.is_open()) { 
+    string line; 
+
+    while (getline(file, line)) { 
+      N++; 
+    }
+}
+
+    file.close();
+    return N;
 }
