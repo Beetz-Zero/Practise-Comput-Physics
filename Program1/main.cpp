2938e7a415e16a563ea80635078b77ac2ac8257d
Beetz <olegson188@gmail.com>
2024-10-24 14:25:52 +1000

    Рефакторинг кода

--- Program1/main.cpp
+++ Program1/main.cpp
@@ -1,28 +1,75 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector> 
 #include <string>
+#include <math.h>
+#include <complex> 
 
 using namespace std;
 
-int main(){
-    ifstream InputFile("234.txt");    
+struct spin{
+    double x;
+    double y;
+    double mx;
+    double my;
+    double junk;
+};
 
+int counter(string x){
+    ifstream InputFile(x);
     string  line;
-    vector<float> numbers; 
-
-    while(getline(InputFile, line)){
-
-        istringstream frag(line);
-        float number;
-
-        while(frag >> number){
-            numbers.push_back(number);
-        }    
-    }    
-    for (float num : numbers) {
-        cout << num << " ";
+    int N = 0;
+    for (int i = 0; getline(InputFile, line); i++)
+    {
+        i++;
+        N += i;
     }
+    return N;
+}
+
+int main(){
+// File initilization
+    ifstream InputFile("XMCD#0000..txt");   
+    ofstream OutputFile("Out.txt"); 
+//------------------------------------------------------------------------------------------------------------//
+// Count number of lines
+    double N = counter("XMCD#0000..txt");
+
+    cout << "Число строк в файле: " << N << endl;  
+//------------------------------------------------------------------------------------------------------------//
+// Filling spin of data
+    string line;
+    
+    spin spin[1100];
+
+    for(int i = 0;getline(InputFile,line);i++){
+        istringstream file(line);
+
+        file >> spin[i].x;
+        file >> spin[i].y;
+        file >> spin[i].mx;
+        file >> spin[i].my;
+        file >> spin[i].junk; 
+    }
+//------------------------------------------------------------------------------------------------------------//
+    double is_eneregy = 0;
+    double sinses = 0;
+    for (double qx = -5; qx < 5; qx += 0.1){
+        for (double qy = -5; qy < 5.1; qy += 0.1){
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+
+                double value = (qx *(spin[i].x - spin[j].x) + qy * (spin[i].y - spin[j].y));
+
+                is_eneregy += (spin[i].mx * spin[j].mx + spin[i].my * spin[j].my) * (cos(value*(180/3.14)));
+
+                sinses = sin(value*(180.0/3.14));
+                }
+            }
+            is_eneregy = is_eneregy * (1/N);
+            OutputFile << is_eneregy<<" "<< qx << " " << qy << " "<< sinses << endl;
+        }
+    }
+
     InputFile.close();
-}   
+}
